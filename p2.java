da0df5e13c69618c142bcafe075f20d2f57385e2
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-02-15 21:57:25 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,17 @@
+public class p2 {
+    public static void main(String[] args) {
+         int a=10;
+         int b=500;
+         int c=20;
+
+         if (a>b && a>c) {
+             System.out.println(a+" max");
+         }else if (b>a && b>c) {
+             System.out.println(b+" max");
+         }if (c>a && c>b) {
+             System.out.println(c+" max");
+         }
+
+         
+    }
+}
