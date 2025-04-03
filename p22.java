0ffaa294318820f9b1dbf641c078efed5b4ba9da
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-03 22:39:57 +0530

    Create p22.java

---
+++ p22.java
@@ -0,0 +1,16 @@
+package pack;
+public class student {
+  public void msg(){system.out.println("Hello");}
+}
+
+student2.java;
+package.mypack;
+import pack.*;
+
+class student2{
+  public static void main(stirng args[])
+  {
+    student obj=new student();
+    obj.msg();
+  }
+}
