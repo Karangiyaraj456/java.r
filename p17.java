3ff0d0c0cd5addc019c2101439bc5886fe8acf9f
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-03 22:31:47 +0530

    Create p17.java

---
+++ p17.java
@@ -0,0 +1,19 @@
+class Animal{
+    public void makesound(){
+        System.out.println("Animal is making a sound");
+    }
+}
+class Dog extends Animal{
+    @Override;
+    public void makesound(){
+        System.out.println("Dog is barking");
+    }
+}
+class methodovering{
+    public static void main(String[] args) {
+        Animal animal=new Animal();
+        animal.makesound();
+        Dog dog=new Dog();
+        dog.makesound();
+    }
+}
