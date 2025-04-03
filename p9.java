06828ea084defa4facf3c3dc87a1af27e25cc413
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-03 22:19:39 +0530

    Create p9.java

---
+++ p9.java
@@ -0,0 +1,14 @@
+class myclass
+  {
+    int cout=3;
+    myclass(){
+      count++;
+      System.out.println(count);
+      }
+    public static void main(string args[])
+    {
+      myclass s1=new myclass();
+      myclass s2=new myclass();
+      myclass s3=new myclass();
+    }
+  }
