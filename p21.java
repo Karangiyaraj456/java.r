764e67e1ec365aacdd6f24c8b30698244426e006
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-03 22:34:30 +0530

    Create p21.java

---
+++ p21.java
@@ -0,0 +1,12 @@
+final class c1{
+    public void f1(){
+        System.out.println("this is final class");
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        c1 obj=new c1();
+        obj.f1();
+    }
+}
+
