6130c7b8e02f478c9ccd7d699a5e9419918a35d7
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-03 22:21:41 +0530

    Create p10.java

---
+++ p10.java
@@ -0,0 +1,8 @@
+ public class main_class{
+     final int count =106 ;
+     public static void main(String[] args) {
+         main_class m = new main_class();
+         System.out.println(m.count);
+     }
+ }
+
