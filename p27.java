71b8124c5c8bcac883a12313c2d68b9e8b47589a
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-04 07:20:05 +0530

    Create p27.java

---
+++ p27.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main
+  {
+    public static void main(String [] args) throw toException
+  {
+    String filepath="sample.txt";
+    filewriter write=newfilewriter(filepath);
+    write.write("Hello this is a simple file handling example");
+    write.close();
+    System.out.println("Data write to file");
+    Buffered Reader(new file Reader(filepath));
+    String line;
+    System.out.println("Reading string line");
+    while(line=readerreadline()=null){
+      System.out.println(line);
+    }
+    header clase();
+  }
+  }
