0e626f081bd053e498295334da79b27a641e350e
Karangiyaraj456 <161800979+Karangiyaraj456@users.noreply.github.com>
2025-04-04 07:33:14 +0530

    Create p29.java

---
+++ p29.java
@@ -0,0 +1,19 @@
+import java.util.Hashset;
+
+public class HashsetExample{
+  public static void main(Stirng[] args){
+    Hashset<String> colors=new Hashset<String>();
+
+    colors.add("Red");
+    colors.add("Bule");
+    colors.add("Green");
+    colors.add("yellow");
+    colors.add("Purple");
+    colors.add("Black");
+
+    System.out.println("colors using in the Hashset :");
+    for(String color :color){
+      Systme.out.println(colors);
+    }  
+  }
+}
